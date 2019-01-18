448fc9ce9b862df8c25e74ce3a6f570ed1c35a1c
Dave <D4D3VD4V3@users.noreply.github.com>
2019-01-18 16:49:53 +0530

    init

---
+++ background.js
@@ -0,0 +1,34 @@
+var width, height;
+
+function openWindow(param) {
+	if (width === undefined)
+		width = 500;
+	if (height === undefined)
+		height = 500;
+	chrome.windows.create({
+		url: "https://en.wikipedia.org/wiki/".concat(param).concat("?mobileaction=toggle_view_mobile"),
+		type: "popup",
+		focused: true,
+		width: parseInt(width, 10),
+		height: parseInt(height, 10)
+	})
+}
+chrome.commands.onCommand.addListener(function (command) {
+
+	chrome.tabs.executeScript({
+		code: "window.getSelection().toString();"
+	}, function (selection) {
+
+		chrome.storage.sync.get(['wiki_width'], function (result) {
+			width = result.wiki_width;
+		});
+		chrome.storage.sync.get(['wiki_height'], function (result) {
+			height = result.wiki_height;
+			openWindow(selection[0]);
+		});
+		console.log(width);
+		console.log(height);
+
+	});
+
+});
---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function load(event) {
+	document.getElementById('save').onclick = function () {
+		var width = document.forms["form1"]["width"].value;
+		var height = document.forms["form1"]["height"].value;
+		chrome.storage.sync.set({
+			wiki_width: width
+		}, function () {
+			console.log('Value is set to ' + width);
+		});
+		chrome.storage.sync.set({
+			wiki_height: height
+		}, function () {
+			console.log('Value is set to ' + height);
+		});
+
+
+	};
+});
